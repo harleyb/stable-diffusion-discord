e23cd97ffd20c74f1fb55b64fe32a970ae0e4fd6
Harley Balabanian <harleyb@mac.com>
2022-11-13 11:00:00 -0500

    fix env name

--- discord_bot.bat
+++ discord_bot.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-call C:\ProgramData\Miniconda3\Scripts\activate.bat %USERPROFILE%\.conda\envs\sd-ldm
+call C:\ProgramData\Miniconda3\Scripts\activate.bat %USERPROFILE%\.conda\envs\ldm
 cd %USERPROFILE%\Documents\stable-diffusion-discord
 python scripts\discord_bot.py
